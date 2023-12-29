46cc88bdb28903bb2556e63e8508968ed40776bb
plazmer <48393153+getplusm@users.noreply.github.com>
2023-12-30 02:04:09 +0500

    Patches

--- main/java/t/me/p1azmer/plugin/dungeons/commands/CommandFlags.java
+++
@@ -1,9 +0,0 @@
-package t.me.p1azmer.plugin.dungeons.commands;
-
-import t.me.p1azmer.engine.api.command.CommandFlag;
-
-public class CommandFlags {
-
-    public static final CommandFlag<Boolean> SILENT = CommandFlag.booleanFlag("s");
-    public static final CommandFlag<Boolean> RANDOM = CommandFlag.booleanFlag("r");
-}
